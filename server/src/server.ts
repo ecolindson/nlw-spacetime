3c4e77f269025f925814a33edd29ae43da983096
Lindson França <lindson@consultoriaeco.com.br>
2023-05-21 10:22:09 -0300

    #3 api

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,20 @@
 import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
+import 'dotenv/config';
 import fastify from 'fastify';
+import { authRoutes } from './routes/auth';
 import { memoriesRoutes } from './routes/memories';
 
 const app = fastify();
 app.register(cors, {
   origin: '*',
 });
+app.register(jwt, {
+  secret: process.env.JWT_SECRET as string,
+});
 app.register(memoriesRoutes);
+app.register(authRoutes);
 
-app.listen({ port: 3000 }).then(() => {
-  console.warn("Bugs' server started at http://localhost:3000/");
+app.listen({ port: 3333 }).then(() => {
+  console.warn("Bugs' server started at http://localhost:3333/");
 });
